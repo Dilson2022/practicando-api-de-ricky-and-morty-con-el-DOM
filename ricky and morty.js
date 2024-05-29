9fa85aedd0d3ec75f3e6e43f5439dbd6835624cb
Dilson2022 <dilsonmasacontreras82@gmail.com>
2024-05-29 00:13:27 -0500

    filtrado

--- ricky and morty.js
+++ ricky and morty.js
@@ -1,46 +1,80 @@
+const boton = document.getElementById("btnPersonaje");
 
-document.addEventListener("DOMContentLoaded", () =>{
-    fetchData()
-})
+let data;
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+  boton.addEventListener("click", (e) => obtenerPersonajesFiltrados(e, data));
+});
+
+//  HACEMOS LA PETICION A LA URL, PARA OBTENER LOS CHARACTER
 const fetchData = async () => {
-    try {
-        loadingData(true)
-        const res = await fetch("https://rickandmortyapi.com/api/character")
-        const data = await res.json()
-        //console.log(data.results);
-        pintarData(data)
-        
-    } catch (error) {
-        console.log( error);
-    } finally{
-        loadingData(false)
-    }
-}
+  try {
+    loadingData(true);
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    data = await res.json();
+    //console.log(data.results);
 
+    pintarData(data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loadingData(false);
+  }
+};
+
+// PINTAMOS TODOS LOS PERSONAJES DE FORMA DINAMICA
 const pintarData = (data) => {
-    const cards = document.getElementById("card-dinamicas")
-    const templateCard = document.getElementById("template-card").content
-    const fragment = document.createDocumentFragment()
-    data.results.forEach(item => {
-        //console.log(item);
-        const clone = templateCard.cloneNode(true)
-        clone.querySelector("h5").textContent = item.name
-        clone.querySelector("p").textContent = item.species
-        clone.querySelector("img").setAttribute("src", item.image)
+  const cards = document.getElementById("card-dinamicas");
+  const templateCard = document.getElementById("template-card").content;
+  const fragment = document.createDocumentFragment();
 
-// Guardamos en el fragment para evitar reflow
-        fragment.appendChild(clone)
-    });
-    cards.appendChild(fragment)
-}
+  // Limpiar todos los elementos dentro de "cards"
+  cards.innerHTML = "";
+
+  data.results.forEach((item) => {
+    //console.log(item);
+    const clone = templateCard.cloneNode(true);
+    clone.querySelector("h5").textContent = item.name;
+    clone.querySelector("p").textContent = item.species;
+    clone.querySelector("img").setAttribute("src", item.image);
+
+    // Guardamos en el fragment para evitar reflow
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
 
 const loadingData = (estado) => {
-    const loading = document.getElementById("loading")
-    if (estado) {
-        loading.classList.remove("d-none")
-    } else {
-        loading.classList.add("d-none")
-    }
-}
-        
+  const loading = document.getElementById("loading");
+  if (estado) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
+};
+
+const filtrarCharacter = (data, species) => {
+  //console.log(data, species);
+  if (data) {
+    return data.results.filter(
+      (character) => character.species.toLowerCase() === species.toLowerCase()
+    );
+  } else {
+    return [];
+  }
+};
+
+const obtenerPersonajesFiltrados = (e, data) => {
+  e.preventDefault(); // Evitar que la página se recargue
+  //console.log(data);
+  const input = document.getElementById("input").value.trim();
+  //console.log(input);
+  if (input !== "") {
+    const personajesFiltrado = filtrarCharacter(data, input);
+    pintarData({ results: personajesFiltrado });
+  } else {
+    // Si el campo está vacío, mostramos todos los personajes nuevamente
+    pintarData(data);
+  }
+};
